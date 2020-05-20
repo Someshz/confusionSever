f9155cb7a935de1108c5b944ee75c42a5101c6c5
somesh <zadesomesh99@gmail.com>
2020-05-20 21:29:21 +0530

    express session part 2

--- app.js
+++ app.js
@@ -13,52 +13,6 @@ const FileStore=require("session-file-store")(session);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const mongoose=require("mongoose");
-function auth (req, res, next) {
-  console.log(req.session);
-
-  if(!req.session.user)
-  {
-    var authHeader = req.headers.authorization;
-  if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-  }
-
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-      req.session.user="admin"
-      next(); 
-  } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      next(err);
-  }
-}
-else{ 
-  if(req.session.user)
-  {
-    next();
-  }
-  else{
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');      
-    err.status = 401;
-    next(err);
-
-  }
-} 
-}
- 
-
-
-
-
 
 var app = express();
 const url="mongodb://localhost:27017/conFusion"
@@ -94,10 +48,33 @@ app.use(session({
   store:new FileStore()
 
 }))
-app.use(auth);
-app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+  console.log(req.session);
+
+if(!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+}
+else {
+  if (req.session.user === 'authenticated') {
+    next();
+  }
+  else {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  }
+}
+}
+app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use("/dishes",dishRouter);
 app.use("/leader",leaderRouter);
 app.use("/promos",promoRouter);

2be43bdc9509a22f6db4a6ff955f43dbb1141652
somesh <zadesomesh99@gmail.com>
2020-07-20 02:17:02 +0530

    oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports={
     "secretKey":"12345-67890-09876-54321",
-    "mongourl":"mongodb://localhost:27017/conFusion"
+    "mongourl":"mongodb://localhost:27017/conFusion",
+    'facebook': {
+        clientId: '971861893267613',
+        clientSecret: '67094b77e412c3cee7dd943dd17792a4'
+    }
 }
